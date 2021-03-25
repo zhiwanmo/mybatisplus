57518264da5ccef30248c611fa2ea8ea84aed491
sxnu_liujun <sxnu_liujun@qq.com>
2021-03-25 14:21:10 +0800

    集成 swagger2

--- src/main/java/com/wanmo/mybatisplus/MybatisplusApplication.java
+++ src/main/java/com/wanmo/mybatisplus/MybatisplusApplication.java
@@ -1,10 +1,7 @@
 package com.wanmo.mybatisplus;
 
-import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MybatisplusApplication {
--- src/main/java/com/wanmo/mybatisplus/controller/UserController.java
+++ src/main/java/com/wanmo/mybatisplus/controller/UserController.java
@@ -2,8 +2,11 @@ package com.wanmo.mybatisplus.controller;
 
 import com.wanmo.mybatisplus.entity.User;
 import com.wanmo.mybatisplus.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,22 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
+@Api(tags = "用户管理模块")
 @RestController
-@RequestMapping("user")
+@RequestMapping("admin/user")
 public class UserController {
 
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping("get/{id}")
+    @ApiOperation(value = "获取单个用户信息")
+    @GetMapping("get/{id}")
     public User get(@PathVariable Long id) {
         User user = userService.getById(id);
         System.out.println(user);
         return user;
     }
 
-    @RequestMapping("list")
+    @ApiOperation(value = "获取所有用户信息")
+    @GetMapping("list")
     public List<User> list() {
         List<User> list = userService.list();
         System.out.println(list);
